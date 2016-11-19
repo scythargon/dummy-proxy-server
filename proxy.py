4a1b39e2f9602cacc4f8ae5380ccaa0c61c4fbe8
Dmitry Vasilev <scythargon@gmail.com>
2016-11-20 01:01:16 +0300

    some fixes, open browser on server start

--- proxy.py
+++ proxy.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from flask import Flask, request
+from flask_script import Manager, Server
 from bs4 import BeautifulSoup
 import requests
 import re
-
-
-app = Flask(__name__)
+import webbrowser
 
 
 def is_visible(element):
@@ -18,8 +16,17 @@ def is_visible(element):
     return True
 
 
+class CustomServer(Server):
+    def __call__(self, app, *args, **kwargs):
+        webbrowser.open('http://127.0.0.1:5000/')
+        return Server.__call__(self, app, *args, **kwargs)
+
+
 site = 'https://habrahabr.ru/'
 what_to_add = u"\u2122"
+
+app = Flask(__name__)
+manager = Manager(app)
 
 
 @app.route('/', defaults={'path': ''})
@@ -28,6 +35,8 @@ def index(path):
     url = site + path
     regexp = re.compile('([^\W\d]{6,})', re.UNICODE)
     resp = requests.get(url)
+    if resp.headers.get('Content-Type') and 'text/html' not in resp.headers.get('Content-Type'):
+        return resp.content
     soup = BeautifulSoup(resp.text, "html.parser")
     strings = soup.findAll(string=regexp)
     visible_strings = filter(is_visible, strings)
@@ -37,5 +46,8 @@ def index(path):
     return str(soup)
 
 
+manager.add_command('runserver', CustomServer(use_reloader=True))
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
